791c7e7fde796481ef735665a0f12edcf6b27d49
xzxiong <xzxiong@yeah.net>
2014-10-13 15:42:23 +0800

    Merge origin/develop into develop
    Conflicts:
    	src/test/Test.java

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,7 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("zmc");
+        System.out.println("zmc xzxiong 修改");
         System.out.println("创建test conflict");
         
         Lyz_Class lyz = new Lyz_Class();
