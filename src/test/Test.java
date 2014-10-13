092d3198a271d3386ae4f4b878c72c6643b35cc3
xzxiong <xzxiong@yeah.net>
2014-10-13 15:28:24 +0800

    xzxiong: con test

--- src/test/Test.java
+++ src/test/Test.java
@@ -2,7 +2,7 @@ package test;
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("创建test");
+        System.out.println("创建test conflict");
         
         Lyz_Class lyz = new Lyz_Class();
         lyz.print();
