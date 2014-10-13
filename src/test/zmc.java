e6774efa016c087b2783f9c89622a8e7838273b0
mc <mc@zmc>
2014-10-13 15:14:35 +0800

    增加zmc类内容1

--- src/test/zmc.java
+++ src/test/zmc.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package test;
 
 /**
@@ -11,8 +10,10 @@ package test;
  * @author mc
  */
 public class zmc {
+
     public static void main(String[] args) {
         System.out.println("增加内容");
-         System.out.println("增加内容2");
+        System.out.println("增加内容2");
+        System.out.println("增加内容3");
     }
 }
